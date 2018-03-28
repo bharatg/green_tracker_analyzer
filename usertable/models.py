4eadef9bf4e1a15b305c0c67a3972ad02a4bf7d9
inknos <nicola.sella@outlook.it>
2018-03-28 10:25:50 +0200

    __str__ method

--- usertable/models.py
+++ usertable/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
 class Users(models.Model):
-    usr_id = models.IntegerField(default=0)
-    ust_name = models.CharField(max_length=200)
+    usr_id   = models.IntegerField(default=0)
+    usr_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return (self.usr_id, self.usr_name)
+    
+class Habits(models.Model):
+    hab_id     = models.IntegerField(default=0)
+    hab_time   = models.DateTimeField('date published')
+    hab_choice = models.CharField(max_length=200)
+
+    def __str__(self):
+        return (self.hab_id, self.hab_time, self.hab_choice)
