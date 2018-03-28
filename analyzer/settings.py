b9df750be0d50a976662b508f390c78ca19af899
inknos <nicola.sella@outlook.it>
2018-03-28 11:36:09 +0200

    env.py

--- analyzer/settings.py
+++ analyzer/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -72,7 +71,11 @@ WSGI_APPLICATION = 'analyzer.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+# Working locally
+# variables stored in env.py file
 import dj_database_url
+
 try:
     import env
     database_url = env.DATABASE_URL
