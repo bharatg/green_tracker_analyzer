291686eb4098092458a1a7bda52b9de41b333b7c
GabrieleLabanca <gabrielelabanca@gmail.com>
2018-05-07 19:57:08 +0200

    weekly view

--- usertable/views.py
+++ usertable/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the usertable index.")
+
+def weekly(request):
+    last_week_list = Habit.query_all()
+    return HttpResponse("You're looking at the weekly view"+last_week_list)
+
+
+
+
